ee11751fc91490bff9309c9aa3a589d70806c6ed
mariarusic <rusic@ualberta.ca>
2026-09-17 16:08:05 -0600

    updated MainActivity

--- code/app/src/main/java/com/example/assignment_zero/MainActivity.kt
+++ code/app/src/main/java/com/example/assignment_zero/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +21,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.SemanticsActions.OnClick
 import androidx.compose.ui.text.style.TextAlign
@@ -68,33 +70,81 @@ fun GreetingPreview() {
 
 // function GiveChanceResult
 //takes in a threshhold int of what is needed for a yes, calculates random chance and returns the result as a 0 (yes) or 1 (no)
-@Composable
-fun giveChanceResult(chance:Int): Int{
+
+fun giveChanceResult(chance:Int): String{
     if(chance > Random.nextInt(0, 100)){
-        return 0
+        return "Yes"
     }
-    return 1
+    return "No"
 }
 //function buttonscreen
 //ui screen, holds buttons
 @Composable
 fun ButtonScreen(){
     var buttonCounter by remember { mutableIntStateOf(0) } //counter init
+    var result by remember{mutableStateOf("Should We Go?")}
 
     Column ( //column for title, result text and counter
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp)
     ){
+        Spacer(modifier = Modifier.height(200.dp))
         //title
-        Text(text="Should I Go?", fontSize =  12.em, textAlign=TextAlign.Center)
+        Text(text=result, fontSize =  12.em, modifier=Modifier.fillMaxWidth(), textAlign=TextAlign.Center)
+
+        Spacer(modifier = Modifier.height(25.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(
+                space=12.dp,
+                alignment = Alignment.CenterHorizontally
+            )
+        ){
+            //button one: yes button (80%)
+            Button(
+                onClick ={
+                    buttonCounter++
+                    result = giveChanceResult(80)
+                }
+            ){
+                Text(text="Sure!")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            //button two: maybe button (50%)
+            Button(
+                onClick ={
+                    buttonCounter++
+                    result = giveChanceResult(50)}
+            ){
+                Text(text="Maybe...")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            //button three: ehh button (15%)
+            Button(
+                onClick ={buttonCounter++
+                    result = giveChanceResult(15)}
+            ){
+                Text(text="Nah.")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(25.dp))
 
         Text(
-            text = "Buttons Pressed: $buttonCounter",
-            fontSize = 32.sp,
-            modifier = Modifier.weight(1f)
+            text = "Total Times Clicked: $buttonCounter times.",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            textAlign=TextAlign.Center
+        )
+        Text(
+            text = "Credit: @ 2026 rusic , 1721833",
+            fontSize = 18.sp
         )
     }
+    /*
     Spacer(modifier = Modifier.height(16.dp))
     Column(
         modifier = Modifier
@@ -132,4 +182,5 @@ fun ButtonScreen(){
     ){//credit column
 
     }
+     */
 }
